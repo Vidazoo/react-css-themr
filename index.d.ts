aa3bf35f494d3cb4761686e0b61543fdd766c7aa
Udi Talias <udi.talias@gmail.com>
2020-08-15 23:14:38 +0300

    fix(client): typeings module name

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-declare module "react-css-themr" {
+declare module "@vidazoo/react-css-themr" {
   type TReactCSSThemrTheme = {
     [key: string]: string | TReactCSSThemrTheme
   }
